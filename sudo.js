78bc762abb1c049ee529174c704986ffd3a5782e
Satoshi Ikeda <ikeda@koozyt.com>
2017-12-12 21:01:19 +0900

    Apply coding style

--- sudo.js
+++ sudo.js
@@ -1,36 +1,42 @@
-"use strict";
+'use strict';
 
 exports = module.exports = sudo;
 
-var spawn = require('child_process').spawn;
-var read = require('read');
-var inpathSync = require('inpath').sync;
-var pidof = require('pidof');
+const spawn = require('child_process').spawn;
+const read = require('read');
+const inpathSync = require('inpath').sync;
+const pidof = require('pidof');
+const path = process.env['PATH'].split(':');
+const sudoBin = inpathSync('sudo', path);
 
-var path = process.env['PATH'].split(':');
-var sudoBin = inpathSync('sudo', path);
+let cachedPassword;
 
-var cachedPassword;
-var lastAnswer;
-
+/**
+ * Run command as root
+ * @param {Array.<string>} command Array of command words
+ * @param {object} options command options
+ * @return {object} Return child_process's spawn result
+ */
 function sudo(command, options) {
-    var prompt = '#node-sudo-passwd#';
-    var prompts = 0;
+    let prompt = '#node-sudo-passwd#';
+    let prompts = 0;
 
-    var args = [ '-S', '-p', prompt ];
-    args.push.apply(args, command);
+    let args = ['-S', '-p', prompt];
+    args.push(...command);
 
     // The binary is the first non-dashed parameter to sudo
-    var bin = command.filter(function (i) { return i.indexOf('-') !== 0; })[0];
+    let bin = command.filter(function(i) {
+        return i.indexOf('-') !== 0;
+    })[0];
 
-    var options = options || {};
-    var spawnOptions = options.spawnOptions || {};
+    options = options || {};
+    let spawnOptions = options.spawnOptions || {};
     spawnOptions.stdio = 'pipe';
 
-    var child = spawn(sudoBin, args, spawnOptions);
+    let child = spawn(sudoBin, args, spawnOptions);
 
     // Wait for the sudo:d binary to start up
-    function waitForStartup(err, pid) {
+    const waitForStartup = (err, pid) => {
         if (err) {
             throw new Error('Couldn\'t start ' + bin);
         }
@@ -38,17 +44,17 @@ function sudo(command, options) {
         if (pid || child.exitCode !== null) {
             child.emit('started');
         } else {
-            setTimeout(function () {
+            setTimeout(function() {
                 pidof(bin, waitForStartup);
             }, 100);
         }
-    }
+    };
     pidof(bin, waitForStartup);
 
     // FIXME: Remove this handler when the child has successfully started
-    child.stderr.on('data', function (data) {
-        var lines = data.toString().trim().split('\n');
-        lines.forEach(function (line) {
+    child.stderr.on('data', function(data) {
+        let lines = data.toString().trim().split('\n');
+        lines.forEach(function(line) {
             if (line === prompt) {
                 if (++prompts > 1) {
                     // The previous entry must have been incorrect, since sudo asks again.
@@ -60,7 +66,12 @@ function sudo(command, options) {
                 } else if (options.cachePassword && cachedPassword) {
                     child.stdin.write(cachedPassword + '\n');
                 } else {
-                    read({ prompt: options.prompt || 'sudo requires your password: ', silent: true }, function (error, answer) {
+                    const args = {
+                        prompt: options.prompt ||
+                            'sudo requires your password: ',
+                        silent: true,
+                    };
+                    read(args, function(error, answer) {
                         child.stdin.write(answer + '\n');
                         if (options.cachePassword) {
                             cachedPassword = answer;
